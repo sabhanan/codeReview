331c5caf3ba3dffd15410f8ea1a092d318690bec
sabhanan <sabhanannaja@gmail.com>
2024-10-29 06:15:56 +0700

    update

--- main.js
+++ main.js
@@ -1,33 +1,33 @@
-// /*
-// !perulangan di javascript
-// ? for, for of, for in, forech
-// *while & do while
-// **/
+/*
+!perulangan di javascript
+? for, for of, for in, forech
+*while & do while
+**/
 
-// for (let i = 1; i <= 5; i++){
-//     console.log('data ke -', i)
-// }
+for (let i = 1; i <= 5; i++){
+    console.log('data ke -', i)
+}
 
-// let x = 1
-// while(x <= 10){
-//     if (x % 2 !== 0){
-//         console.log('data x ke - ', x)
-//     }
-//     x++
-// }
-
-
-// let y = 1
-// do {
-//     if (y % 2 !== 0){
-//         console.log('data x ke - ', y)
-//     }
-//     y++
-// } while (y <= 10)
+let x = 1
+while(x <= 10){
+    if (x % 2 !== 0){
+        console.log('data x ke - ', x)
+    }
+    x++
+}
 
 
-// const pacarSaya = ['agnes', 'angel', 'zola', 'zapi', 'veva']
+let y = 1
+do {
+    if (y % 2 !== 0){
+        console.log('data x ke - ', y)
+    }
+    y++
+} while (y <= 10)
 
-// pacarSaya.forEach((pacar, index) => {
-//     console.log(pacar, index + 1)
-// })
+
+const pacarSaya = ['agnes', 'angel', 'zola', 'zapi', 'veva']
+
+pacarSaya.forEach((pacar, index) => {
+    console.log(pacar, index + 1)
+})
